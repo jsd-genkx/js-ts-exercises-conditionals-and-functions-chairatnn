7b9a9dbda2e67aa415f3b52a7afa670a8fb5f478
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-11-19 15:53:12 +0700

    solution task01,02

--- task01.ts
+++ task01.ts
@@ -1,3 +1,22 @@
 // TODO-01: Use if/else to print "Even" or "Odd"
 
 const number = 15;
+// console.log(number);
+
+// My solution
+let a = number%2;
+if (a = 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+
+// Solution by K.Juang 
+// % คือ modulo operator → หารเอาเศษ
+// if ( number % 2 === 0 ) {
+//     console.log("Even");
+// }
+// else
+//     {console.log("Odd");
+// }
--- task02.ts
+++ task02.ts
@@ -3,3 +3,31 @@
 
 const userInput = "42";
 // Try changing to "hello", 10, null, NaN
+
+// Solution by K.ITT
+if (typeof userInput === 'number' && !isNaN(userInput)){
+    console.log(`T :${userInput} <- This is Number`);
+}else{
+    console.log(`F :${userInput} <-This is not number /is NaN`);
+}
+
+// Solution by Gemini
+// if (typeof userInput === 'number') {
+     // กรณีที่ 1: เป็น number ตรงๆ (เช่น 10)
+//     if (isNaN(userInput as number)) {
+//         console.log("Input is NaN (Not a Number) - not a valid number.");
+//     } else {
+//         console.log(`Input is a valid number: ${userInput}`);
+//     }
+// } else if (typeof userInput === 'string') {
+     // กรณีที่ 2: เป็น string ลองแปลงเป็น number ก่อน
+//     const parsedNumber = Number(userInput);
+//     if (isNaN(parsedNumber)) {
+//         console.log(`Input is a string ("${userInput}") that cannot be converted to a number.`);
+//     } else {
+//         console.log(`Input is a string ("${userInput}") successfully converted to number: ${parsedNumber}`);
+//     }
+// } else {
+     // กรณีที่ 3: เป็น type อื่นๆ ที่ไม่ต้องการ
+//     console.log(`Input is not a number or a convertible string. Type is: ${typeof userInput}`);
+// }
