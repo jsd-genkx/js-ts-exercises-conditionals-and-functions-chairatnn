cb4687a380e9613289d33ba7edd8011d03f36bde
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-11-19 17:40:44 +0700

    solution task03,04

--- task03.ts
+++ task03.ts
@@ -3,4 +3,15 @@
 // 6–10 chars → "Moderate"
 // 11+ chars → "Strong"
 
-const password = "pass123";
+const password: string = "pass123";
+
+// My Solution
+const letter = password.length; // นับความยาวตัวอักษรของรหัสผ่าน
+
+if (letter < 6) {
+    console.log(`Password: "${password}" (${letter} chars) -> Too short`); // ความยาวน้อยกว่า 6 ตัวอักษรให้พิมพ์
+} else if (letter >= 6 && letter <= 10) {
+    console.log(`Password: "${password}" (${letter} chars) -> Moderate`);   // หากความยาว 6 ถึง 10 ตัวอักษรให้พิมพ์
+} else {
+    console.log(`Password: "${password}" (${letter} chars) -> Strong`); // หากความยาว >= 11 ให้พิมพ์
+}
--- task04.ts
+++ task04.ts
@@ -7,3 +7,14 @@
 // TODO BONUS: Rewrite with if
 
 const num = 15;
+
+// My Solution
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("FizzBuzz");    // หาร 3 และ 5 ลงตัว
+} else if (num % 3 === 0) {
+    console.log("Fizz");        // หารด้วย 3 ลงตัว
+} else if (num % 5 === 0) {
+    console.log("Buzz");        // หารด้วย 5 ลงตัว
+} else {
+    console.log("Not divisible by 3 or 5");     // หารด้วย 3 หรือ 5 ไม่ลงตัว
+}
